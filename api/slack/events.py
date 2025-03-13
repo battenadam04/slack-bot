320587c713af444761b77f5e309df022b7c88dc2
Adam Batten <adambatten@deloitte.co.uk>
2025-03-13 11:31:43 +0000

    removing redundant code

--- api/slack/events.py
+++ api/slack/events.py
@@ -40,34 +40,9 @@ def slack_events():
         return jsonify(challenge_response).encode(), 200
     
     # Verification should happen before data processing
-    # if not signature_verifier.is_valid_request(data, request.headers): 
-    #     return jsonify({'status': 'invalid_request'}), 403
+    if not signature_verifier.is_valid_request(data, request.headers): 
+        return jsonify({'status': 'invalid_request'}), 403
 
-    # Assuming you're sending JSON
-    event = request.get_json() 
-
-    if event is not None:
-        print(f"Event type: {event.get('type')}, Event Callback Id: {event.get('callback_id')}") 
-
-        if event.get('type') == 'shortcut' and event.get('callback_id') == 'buddy_up':
-            user_id = event.get('user', {}).get('id') 
-
-            if user_id:
-                print(f"Posting a message to: {event}")
-                try:
-                    web_client.chat_postMessage(
-                        channel=user_id,
-                        text=f"Hello <@{user_id}>! You invoked the Buddy Up shortcut."
-                    )
-                    return jsonify({'status': 'ok'}), 200
-                except SlackApiError as e:
-                    return jsonify({"status": "error", "message": str(e)}), 500
-            else:
-                return jsonify({"status": "error", "message": "missing_user_id"}), 400 
-        else:
-            return jsonify({"status": "error", "message": "unknown_event"}), 400
-    else:
-        return jsonify({"status": "error", "message": "missing_payload"}), 400
 
 if __name__ == "__main__":
     app.run(port=3000) 
